e33d0a4fe041bb1bb806f583abaebf3539223b5d
Julia Herbsthofer <julia.herbsthofer@student.tugraz.at>
2023-08-09 17:30:07 +0200

    ui test

---
+++ build.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+
+fn main() -> Result<(),()>{
+    println!("cargo:rerun-if-changed=ui");
+    println!("cargo:warning=detected UI update, rebuilding");
+    match Command::new("trunk").current_dir("./ui").arg("build").arg("--release").output() {
+        Ok(_) => {
+            match Command::new("cp").arg("-R").arg("./ui/dist").arg(".").output() {
+                Ok(_) => Ok(()),
+                Err(e) => {
+                    println!("cargo:warning=❌ Failed fetching dist files: {}", e);
+                    Err(())
+                },
+            }
+        },
+        Err(e) => {
+            println!("cargo:warning=❌ Failed to run trunk: {} ", e);
+            println!("cargo:warning=❗ install trunk using \x1b[1mcargo install trunk\x1b[0m");
+            Err(())
+        }
+    }
+}
